32f9260c8e22eb4354d4c9fe1b3a735f9c5b633b
snahid <nahidchawdhury77@gmail.com>
2021-07-30 19:24:00 +0600

    done

--- config.js
+++ config.js
@@ -1,12 +1,7 @@
-$(function() {
+localStorage.setItem('firstVisit', 1);
+let firstView = localStorage.getItem("firstVisit");
+// console.log(firstView);
 
-    $('[data-toggle="modal"]').hover(function() {
-      var modalId = $(this).data('target');
-      $(modalId).modal('show');
-  
-    });
-  
-  });
 
 
   let loader = document.getElementById("preloader");
@@ -37,8 +32,8 @@ $(function() {
     console.log(bodyH);
     let fullPageHeight = bodyH;
     let page = Math.round(bodyH/viewHeight);
-    console.log("pageHeight " + bodyH);
-    console.log("Page " + page);
+    // console.log("pageHeight " + bodyH);
+    // console.log("Page " + page);
     // Get Height
 
         for (let i=1; i<=page; i++) {
@@ -53,9 +48,7 @@ $(function() {
             div.style.position = "absolute";
             div.style.zIndex = 3;
         }
-
-
-       
+      
         // page 1 
         let id1 = document.getElementById("main-1");
         id1.style.top = 0;
@@ -143,44 +136,38 @@ $(function() {
           // page 7 
 
           // page 8 
-          let id8 = document.getElementById("main-8");
-          id8.style.top = viewHeight*7+"px";
-          id8.style.left = 0;
-          id8.style.zIndex = 1;
-  
-          let elemnt8 = document.createElement("span");
-          elemnt8.setAttribute('id', "page-8");
-          elemnt8.style.opacity = 0;
-          id8.appendChild(elemnt8);
+          // let id8 = document.getElementById("main-8");
+          // id8.style.top = viewHeight*7+"px";
+          // id8.style.left = 0;
+          // id8.style.zIndex = 1;
+          
+          // let elemnt8 = document.createElement("span");
+          // elemnt8.setAttribute('id', "page-8");
+          // elemnt8.style.opacity = 0;
+          // id8.appendChild(elemnt8);
           // page 8 
 
         //  // page 9 
-        //    let id9 = document.getElementById("main-9");
-        //    id9.style.top = viewHeight*8+"px";
-        //    id9.style.left = 0;
-        //    id9.style.zIndex = 1;
+          //  let id9 = document.getElementById("main-9");
+          //  id9.style.top = viewHeight*8+"px";
+          //  id9.style.left = 0;
+          //  id9.style.zIndex = 1;
    
-        //    let elemnt9 = document.createElement("span");
-        //    elemnt9.setAttribute('id', "page-9");
-        //    id9.appendChild(elemnt9);
+          //  let elemnt9 = document.createElement("span");
+          //  elemnt9.setAttribute('id', "page-9");
+          //  id9.appendChild(elemnt9);
         //  // page 9 
 
         //   // page 10 
-        //    let id10 = document.getElementById("main-10");
-        //    id10.style.top = viewHeight*9+"px";
-        //    id10.style.left = 0;
-        //    id10.style.zIndex = 1;
+          //  let id10 = document.getElementById("main-10");
+          //  id10.style.top = viewHeight*9+"px";
+          //  id10.style.left = 0;
+          //  id10.style.zIndex = 1;
    
-        //    let elemnt10 = document.createElement("span");
-        //    elemnt10.setAttribute('id', "page-10");
-        //    id10.appendChild(elemnt10);
+          //  let elemnt10 = document.createElement("span");
+          //  elemnt10.setAttribute('id', "page-10");
+          //  id10.appendChild(elemnt10);
         //  //  page 10 
-
-
-       
-
-
-
 
 
   function ViewPort() {
@@ -197,12 +184,6 @@ $(function() {
     console.log("Your View Port Size is:" + viewsize);
   }
   ViewPort();
-
-
-  let time1,time2,time3,time4,time5;
-
-  
-
 
 //   Time Count 
       TimeMe.initialize({
@@ -234,9 +215,11 @@ $(function() {
         TimeMe.trackTimeOnElement("main-5");
         TimeMe.trackTimeOnElement("main-6");
         TimeMe.trackTimeOnElement("main-7");
-        TimeMe.trackTimeOnElement("main-8");
+        // TimeMe.trackTimeOnElement("main-8");
         // TimeMe.trackTimeOnElement("main-9");
         // TimeMe.trackTimeOnElement("main-10");
+
+
         setInterval(function () {
           let timeSpentOnPage = TimeMe.getTimeOnCurrentPageInSeconds();
           document.getElementById("timeInSeconds").textContent =
@@ -252,16 +235,12 @@ $(function() {
           //   document.getElementById("activityStatus").textContent =
           //     "You have left the page.";
           // }
-
           let timeSpentOnElement =
             TimeMe.getTimeOnElementInSeconds("main-1");
-          document.getElementById("page-1").textContent =
+            document.getElementById("page-1").textContent =
             timeSpentOnElement.toFixed(2);
-
+          
          
-
-        
-
           let timeSpentOnElement2 =
             TimeMe.getTimeOnElementInSeconds("main-2");
           document.getElementById("page-2").textContent =
@@ -282,11 +261,11 @@ $(function() {
           document.getElementById("page-5").textContent =
             timeSpentOnElement5.toFixed(2);
 
-            time1 = timeSpentOnElement;
-            time2= timeSpentOnElement2;
-            time3= timeSpentOnElement3;
-            time4= timeSpentOnElement4;
-            time5= timeSpentOnElement5;
+            // time1 = timeSpentOnElement;
+            // time2= timeSpentOnElement2;
+            // time3= timeSpentOnElement3;
+            // time4= timeSpentOnElement4;
+            // time5= timeSpentOnElement5;
 
         
 
@@ -300,10 +279,10 @@ $(function() {
           document.getElementById("page-7").textContent =
             timeSpentOnElement7.toFixed(2);
 
-            let timeSpentOnElement8 =
-            TimeMe.getTimeOnElementInSeconds("main-8");
-          document.getElementById("page-8").textContent =
-            timeSpentOnElement8.toFixed(2);
+          //   let timeSpentOnElement8 =
+          //   TimeMe.getTimeOnElementInSeconds("main-8");
+          // document.getElementById("page-8").textContent =
+          //   timeSpentOnElement8.toFixed(2);
 
           //   let timeSpentOnElement9 =
           //   TimeMe.getTimeOnElementInSeconds("main-9");
@@ -318,92 +297,199 @@ $(function() {
       };
 //   Time Count 
 
+// Save Data 
+let div1 = document.getElementById("main-1");
+let div2 = document.getElementById("main-2");
+let div3 = document.getElementById("main-3");
+let div4 = document.getElementById("main-4");
+let div5 = document.getElementById("main-5");
+let div6 = document.getElementById("main-6");
+let div7 = document.getElementById("main-7");
+let mypage1,mypage2,mypage3,mypage4,mypage5,mypage6,mypage7;
+// Page 1 
+div1.addEventListener("mouseleave", function(){
+  mypage1 = document.getElementById("timeInSeconds").innerHTML;
+  if(firstView == 1){
+    if(localStorage["page1"]){
+      let rpage1 = localStorage.getItem('page1');
+      localStorage.setItem("page1", parseInt(parseInt(mypage1)+parseInt(rpage1)));
+        // console.log(rpage1);
+        // console.log('asifff');
+        // console.log(mypage);
+    }else{
+      localStorage.setItem("page1", mypage1);
+    }
+  }else{
+    localStorage.setItem("page1", mypage1);
+  }
+});
+// Page 1 
 
+// Page 2 
+div2.addEventListener("mouseleave", function(){
+  mypage2 = document.getElementById("page-2").innerHTML;
+  if(firstView == 1){
+    if(localStorage["page2"]){
+      let rpage2 = localStorage.getItem('page2');
+      localStorage.setItem("page2", parseInt(parseInt(mypage2)+parseInt(rpage2)));
+        // console.log(rpage1);
+        // console.log('asifff');
+        // console.log(mypage);
+    }else{
+      localStorage.setItem("page2", mypage2);
+    }
+  }else{
+    localStorage.setItem("page2", mypage2);
+  }
+});
+// Page 2
 
+// Page 3 
+div3.addEventListener("mouseleave", function(){
+  mypage3 = document.getElementById("page-3").innerHTML;
+  if(firstView == 1){
+    if(localStorage["page3"]){
+      let rpage3 = localStorage.getItem('page3');
+      localStorage.setItem("page3", parseInt(parseInt(mypage3)+parseInt(rpage3)));
+        // console.log(rpage1);
+        // console.log('asifff');
+        // console.log(mypage);
+    }else{
+      localStorage.setItem("page3", mypage3);
+    }
+  }else{
+    localStorage.setItem("page3", mypage3);
+  }
+});
+// Page 3
 
+// Page 4 
+div4.addEventListener("mouseleave", function(){
+  mypage4 = document.getElementById("page-4").innerHTML;
+  if(firstView == 1){
+    if(localStorage["page4"]){
+      let rpage4 = localStorage.getItem('page4');
+      localStorage.setItem("page4", parseInt(parseInt(mypage4)+parseInt(rpage4)));
+        // console.log(rpage1);
+        // console.log('asifff');
+        // console.log(mypage);
+    }else{
+      localStorage.setItem("page4", mypage4);
+    }
+  }else{
+    localStorage.setItem("page4", mypage4);
+  }
+});
+// Page 4
 
+// Page 5 
+div5.addEventListener("mouseleave", function(){
+  mypage5 = document.getElementById("page-5").innerHTML;
+  if(firstView == 1){
+    if(localStorage["page5"]){
+      let rpage5 = localStorage.getItem('page5');
+      localStorage.setItem("page5", parseInt(parseInt(mypage5)+parseInt(rpage5)));
+        // console.log(rpage1);
+        // console.log('asifff');
+        // console.log(mypage);
+    }else{
+      localStorage.setItem("page5", mypage5);
+    }
+  }else{
+    localStorage.setItem("page5", mypage5);
+  }
+});
+// Page 5
+
+// Page 6 
+div6.addEventListener("mouseleave", function(){
+  mypage6 = document.getElementById("page-6").innerHTML;
+  if(firstView == 1){
+    if(localStorage["page6"]){
+      let rpage6 = localStorage.getItem('page6');
+      localStorage.setItem("page6", parseInt(parseInt(mypage6)+parseInt(rpage6)));
+        // console.log(rpage1);
+        // console.log('asifff');
+        // console.log(mypage);
+    }else{
+      localStorage.setItem("page6", mypage6);
+    }
+  }else{
+    localStorage.setItem("page6", mypage6);
+  }
+});
+// Page 6
+
+// Page 7 
+div7.addEventListener("mouseleave", function(){
+  mypage7 = document.getElementById("page-7").innerHTML;
+  if(firstView == 1){
+    if(localStorage["page7"]){
+      let rpage7 = localStorage.getItem('page7');
+      localStorage.setItem("page7", parseInt(parseInt(mypage7)+parseInt(rpage7)));
+        // console.log(rpage1);
+        // console.log('asifff');
+        // console.log(mypage);
+    }else{
+      localStorage.setItem("page7", mypage7);
+    }
+  }else{
+    localStorage.setItem("page7", mypage7);
+  }
+});
+// Page 7
+
+// Save Data 
 
 //  Graph 
-var ctx = document.getElementById("myChart").getContext("2d");
-var myChart = new Chart(ctx, {
-    type:"bar",
-    data:{
-        labels:["1", "2", "3", "4", "5", "6", "7", "8", "9", "10" ],
-        datasets:[
-            {
-                label: "Time Counting",
-                data:[500, 100, 4, 50, 1000, 7, 100, 3, 1500, 6],
-                backgroundColor:[
-                    "#3a7af2","#3a7af2","#3a7af2","#3a7af2","#3a7af2","#3a7af2","#3a7af2","#3a7af2","#3a7af2","#3a7af2"
-                ]
-            },
-        ],
+
+var myChart;
+function showGraph() {
+  var ctx = document.getElementById("myChart").getContext("2d");
+  if (myChart) myChart.destroy();
+  myChart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: ["1", "2", "3", "4", "5", "6", "7"],
+      datasets: [
+        {
+          label: "Time Counting",
+          data: [localStorage.getItem('page1'), localStorage.getItem('page2'), localStorage.getItem('page3'), localStorage.getItem('page4'), localStorage.getItem('page5'), localStorage.getItem('page6'), localStorage.getItem('page7')],
+          backgroundColor: [
+            "#3a7af2", "#3a7af2", "#3a7af2", "#3a7af2", "#3a7af2", "#3a7af2", "#3a7af2", "#3a7af2", "#3a7af2", "#3a7af2"
+          ]
+        },
+      ],
     },
-    options:{
-      title:{
-        display:true,
+    options: {
+      title: {
+        display: true,
         text: "Page",
         fontSize: 16,
         position: "bottom",
         fontColor: '#292929'
       },
-        layout:{
-          padding:{
-            left: 40,
-            top: 0,
-            bottom: 0,
-            right: 0,
-          },
+      layout: {
+        padding: {
+          left: 40,
+          top: 0,
+          bottom: 0,
+          right: 0,
         },
-      //   scales:{
-      //     xAxes: [{
-      //         display: false //this will remove all the x-axis grid lines
-      //     }]
-      // },
-     },
+      },
+
+    },
+  });
+  myChart.update();
+}
+
+$(function () {
+
+  $('[data-toggle="modal"]').hover(function () {
+    showGraph();
+    var modalId = $(this).data('target');
+    $(modalId).modal('show');
+  });
+
 });
-
-
-
-// var ctx = document.getElementById('myChart').getContext('2d');
-//         var myChart = new Chart(ctx, {
-//             // The type of chart we want to create
-//             type: 'doughnut',
-
-//             // The data for our dataset
-//             data: {
-//                 labels: ['Treats', 'Food', 'Toys'],
-//                 datasets: [{
-//                     label: "Score",
-//                     data: [45, 20, 35],
-//                     backgroundColor: [
-//                         "#F36BCB",
-//                         "#8D49E6",
-//                         "#10ADFF"
-//                     ]
-//                 }]
-//             },
-
-//             // Configuration options go here
-//             options: {
-//                 responsive: false,
-//                 cutoutPercentage: 65,
-//                 title: {
-//                     display: true,
-//                     position: "bottom",
-//                     text: "Top Products",
-//                     fontSize: 18,
-//                     fontColor: "#111"
-//                 },
-//                 legend: {
-//                     display: true,
-//                     position: "top",
-//                     labels: {
-//                         fontColor: "#1E3E59",
-//                         fontSize: 16,
-//                         padding: 10
-//                     }
-//                 }
-//             }
-//         });
-// Graph 
+// Graph
